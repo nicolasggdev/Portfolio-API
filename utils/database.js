ad2e1473773d29ae20e71caffdfd8c75d818ae1c
nicolasggdev <nicolasggdev@gmail.com>
2023-03-08 19:49:46 -0500

    Change the deployment server

--- utils/database.js
+++ utils/database.js
@@ -7,17 +7,17 @@ dotenv.config({ path: "./config.env" });
 const database = new Sequelize({
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
-  password: "" + process.env.DB_PASSWORD,
-  database: process.env.DB,
+  password: process.env.DB_PASSWORD,
+  // database: process.env.DB,
   port: process.env.DB_PORT,
   dialect: "postgres",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+  logging: false
+  // dialectOptions: {
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false
+  //   }
+  // }
 });
 
 module.exports = { database };
